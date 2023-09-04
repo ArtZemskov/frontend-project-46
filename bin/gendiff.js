9f89339814f36c67211f41d7abdd20338cf03ef1
Артем Земсков <artem_zemskov@MacBook-Air.local>
2023-09-04 15:00:58 +0200

    update file

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,7 +3,9 @@ import { Command } from "commander";
 const program = new Command();
 
 program
-  .description
-  .version
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>')
 
 program.parse();
