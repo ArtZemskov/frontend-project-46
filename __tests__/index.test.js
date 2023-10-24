cc85fb7bff48be4be85d83990c495669611f41ff
Артем Земсков <artem_zemskov@MacBook-Air.home>
2023-10-24 11:22:57 +0200

    fix

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -6,18 +6,32 @@ import genDiff from '../src/index.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-test('difference', () => {
-  const pathToTest = path.join(__dirname, '..', '__fixtures__', 'test1.txt');
-  const result = fs.readFileSync(pathToTest, 'utf-8');
-  const pathToFile1 = path.join(__dirname, '..', '__fixtures__', 'file1.json');
-  const pathToFile2 = path.join(__dirname, '..', '__fixtures__', 'file2.json');
-  expect(genDiff(pathToFile1, pathToFile2)).toEqual(result);
-});
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+// const readFile = (filename) => fs.readFile(getFixturePath(filename), 'utf-8');
 
-test('diffYaml', () => {
-  const pathToTest = path.join(__dirname, '..', '__fixtures__', 'test1.txt');
-  const result = fs.readFileSync(pathToTest, 'utf-8');
-  const pathToFile1 = path.join(__dirname, '..', '__fixtures__', 'file1.yaml');
-  const pathToFile2 = path.join(__dirname, '..', '__fixtures__', 'file2.yaml');
-  expect(genDiff(pathToFile1, pathToFile2)).toEqual(result);
+// test('difference', () => {
+//   const pathToTest = path.join(__dirname, '..', '__fixtures__', 'test1.txt');
+//   const result = fs.readFileSync(pathToTest, 'utf-8');
+//   const pathToFile1 = path.join(__dirname, '..', '__fixtures__', 'file1.json');
+//   const pathToFile2 = path.join(__dirname, '..', '__fixtures__', 'file2.json');
+//   expect(genDiff(pathToFile1, pathToFile2)).toEqual(result);
+// });
+
+// test('diffYaml', () => {
+//   const pathToTest = path.join(__dirname, '..', '__fixtures__', 'test1.txt');
+//   const result = fs.readFileSync(pathToTest, 'utf-8');
+//   const pathToFile1 = path.join(__dirname, '..', '__fixtures__', 'file1.yaml');
+//   const pathToFile2 = path.join(__dirname, '..', '__fixtures__', 'file2.yaml');
+//   expect(genDiff(pathToFile1, pathToFile2)).toEqual(result);
+// });
+
+test.each([
+  { file1: 'file1.json', file2: 'file2.json', expected: 'test1.txt' },
+  { file1: 'file1.yaml', file2: 'file2.yaml', expected: 'test1.txt' },
+])('diff', ({ file1, file2, expected }) => {
+  const pathToFile1 = getFixturePath(file1);
+  const pathToFile2 = getFixturePath(file2);
+  const result = getFixturePath(expected);
+  const readFile = fs.readFileSync(result, 'utf-8');
+  expect(genDiff(pathToFile1, pathToFile2)).toEqual(readFile);
 });
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import path from 'path';
 import fs from 'fs';
 import _ from 'lodash';
 import parser from './parsers/parser.js';
+import formatter from './formatters/index.js';
+import buildTree from './buildTree.js';
 
 const genDiff = (path1, path2) => {
   const resolvedPath1 = path.resolve(cwd(), path1);
@@ -12,27 +14,8 @@ const genDiff = (path1, path2) => {
 
   const obj1 = parser(data1, path.extname(path1).slice(1));
   const obj2 = parser(data2, path.extname(path2).slice(1));
-  const keys1 = Object.keys(obj1);
-  const keys2 = Object.keys(obj2);
-  const uniqueSortedKeys = _.sortBy(Array.from(new Set([...keys1, ...keys2])));
-
-  const newObject = uniqueSortedKeys.reduce((acc, key) => {
-    if (!Object.hasOwn(obj1, key)) {
-      acc[`  + ${key}`] = obj2[key];
-    } else if (!Object.hasOwn(obj2, key)) {
-      acc[`  - ${key}`] = obj1[key];
-    } else if (obj1[key] !== obj2[key]) {
-      acc[`  - ${key}`] = obj1[key];
-      acc[`  + ${key}`] = obj2[key];
-    } else {
-      acc[`    ${key}`] = obj1[key];
-    }
-    return acc;
-  }, {});
-  const result = Object.keys(newObject)
-    .map((key) => `${key}: ${newObject[key]}`)
-    .join('\n');
-  return `{\n${result}\n}`;
+  const tree = buildTree(obj1, obj2);
+  const result = formatter(tree, 'stylish');
+  return result;
 };
-// console.log(genDiff('file1.json', 'file2.json'));
 export default genDiff;
