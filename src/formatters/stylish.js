8c88face6445faeb003fdb89b315e9b8e7ed873f
Артем Земсков <artem_zemskov@MacBook-Air.home>
2023-10-24 11:30:04 +0200

    added new logic by dividing src/index.js

---
+++ src/formatters/stylish.js
@@ -0,0 +1,16 @@
+const stylish = (tree) => {
+    const result = tree.reduce((acc, item) => {
+        if (item.status === 'added') {
+            acc.push(`  + ${item.key}: ${item.value}`);
+        } else if (item.status === 'deleted') {
+            acc.push(`  - ${item.key}: ${item.value}`);
+        } else if (item.status === 'changed') {
+            acc.push(`  - ${item.key}: ${item.value1}\n  + ${item.key}: ${item.value2}`);
+        } else {
+            acc.push(`    ${item.key}: ${item.value}`);
+        }
+        return acc;
+    }, []);
+    return `{\n${result.join('\n')}\n}`;
+};
+export default stylish;
