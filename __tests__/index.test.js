1a1403f2c504bd35e88015bbb393efa9df8e1a21
Артем Земсков <artem_zemskov@MacBook-Air.home>
2024-02-15 15:47:09 +0100

    update

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,12 +9,14 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 test.each([
-  { file1: 'file1.json', file2: 'file2.json', expected: 'stylish.txt' },
-  { file1: 'file1.yaml', file2: 'file2.yaml', expected: 'stylish.txt' },
-])('diff', ({ file1, file2, expected }) => {
+  { file1: 'file1.json', file2: 'file2.json', format: 'stylish', expected: 'stylish.txt' },
+  { file1: 'file1.yaml', file2: 'file2.yaml', format: 'stylish', expected: 'stylish.txt' },
+  { file1: 'file1.json', file2: 'file2.json', format: 'plain', expected: 'plain.txt' },
+  { file1: 'file1.yaml', file2: 'file2.yaml', format: 'plain', expected: 'plain.txt' },
+])('diff', ({ file1, file2, format, expected }) => {
   const pathToFile1 = getFixturePath(file1);
   const pathToFile2 = getFixturePath(file2);
   const result = getFixturePath(expected);
   const readFile = fs.readFileSync(result, 'utf-8');
-  expect(genDiff(pathToFile1, pathToFile2)).toEqual(readFile);
+  expect(genDiff(pathToFile1, pathToFile2, format)).toEqual(readFile);
 });
