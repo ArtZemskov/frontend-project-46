5058dfb24b6a2a82f250125e73c7d2b76216bb68
Артем Земсков <artem_zemskov@MacBook-Air.home>
2024-02-15 13:26:46 +0100

    update

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,8 +9,8 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 test.each([
-  { file1: 'file1.json', file2: 'file2.json', expected: 'test1.txt' },
-  { file1: 'file1.yaml', file2: 'file2.yaml', expected: 'test1.txt' },
+  { file1: 'file1.json', file2: 'file2.json', expected: 'stylish.txt' },
+  { file1: 'file1.yaml', file2: 'file2.yaml', expected: 'stylish.txt' },
 ])('diff', ({ file1, file2, expected }) => {
   const pathToFile1 = getFixturePath(file1);
   const pathToFile2 = getFixturePath(file2);
--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,9 +1,12 @@
+import plain from './plain.js';
 import stylish from './stylish.js';
 
 const formatter = (arr, format) => {
   switch (format) {
     case 'stylish':
       return stylish(arr);
+    case 'plain':
+      return plain(arr);
     default:
       throw new Error('wrong format');
   }
