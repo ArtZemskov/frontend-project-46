66db87983620c701a429e0c8f7cc1960d73f8975
Артем Земсков <artem_zemskov@MacBook-Air.home>
2024-02-15 14:07:52 +0100

    refactor

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -8,28 +8,26 @@ const srtingify = (value) => {
 };
 
 const plain = (tree, prevKeys = []) => {
-  const list = tree.map((node) => {
-    const {
-      key, children, value, value1, value2, status,
-    } = node;
-    const keys = [...prevKeys, key];
-    const path = keys.join('.');
-    if (status === 'nested') {
-      return plain(children, keys);
-    }
-    if (status === 'added') {
-      return `Property '${path}' was added with value: ${srtingify(value)}`;
-    }
-    if (status === 'deleted') {
-      return `Property '${path}' was removed`;
-    }
-    if (status === 'changed') {
-      return `Property '${path}' was updated. From ${srtingify(value1)} to ${srtingify(value2)}`;
-    }
-  });
-
-  const result = list.filter((item) => item !== undefined);
-
+  const result = tree.filter((node) => node.status !== 'unchanged')
+    .map((node) => {
+      const {
+        key, children, value, value1, value2, status,
+      } = node;
+      const keys = [...prevKeys, key];
+      const path = keys.join('.');
+      switch (status) {
+        case 'nested':
+          return plain(children, keys);
+        case 'added':
+          return `Property '${path}' was added with value: ${srtingify(value)}`;
+        case 'deleted':
+          return `Property '${path}' was removed`;
+        case 'changed':
+          return `Property '${path}' was updated. From ${srtingify(value1)} to ${srtingify(value2)}`;
+        default:
+          throw new Error(`Unknown status ${status}`);
+      }
+    });
   return `${result.join('\n')}`;
 };
 export default plain;
